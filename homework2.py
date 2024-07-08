d2a8de2d8fece816ee3d5c2482323fff6302334d
Maytreya777 <maytreya777@gmail.com>
2024-07-08 12:24:39 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,9 @@
+homework_count = 12
+o_clok_count = 1.5
+class_name = 'Python'
+exercise_time = (homework_count /o_clok_count )
+print(homework_count)# 12
+print(o_clok_count) # 1.5
+print(class_name) # Python
+print(homework_count /o_clok_count)
+print(class_name,', всего задач: ',homework_count,', затрачено часов: ',o_clok_count,', среднее время выполнения ', homework_count /o_clok_count, 'часа' )#Python
